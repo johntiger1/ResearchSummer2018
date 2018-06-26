48d5c64bb4935ffbdf01f8c44834ae8bd73759b3
John Chen <john.person1@gmail.com>
2018-06-26 10:32:59 -0400

    demonstrate gensim simple_preprocess

--- testfile.py
+++ testfile.py
@@ -11,23 +11,24 @@ if __name__ == "__main__":
     # nlp = spacy.load('en_core_web_md')
     # hardcoded for now
 
-    for filename in glob.iglob('../pubmed_data/**/*.txt', recursive=True):
-        print(filename)
-
-    exit()
 
 
-    for filename in os.listdir("./pubmed_data/unzipped/AAPS_J"):
-        with open(os.path.join("./pubmed_data/unzipped/AAPS_J", filename)) as file:
+    # exit()
+
+
+    for filename in os.listdir("../pubmed_data/unzipped/AAPS_J"):
+        with open(os.path.join("../pubmed_data/unzipped/AAPS_J", filename)) as file:
             print(filename)
             text = file.read()
+
+            # we should try doing things with gensim simple preprocess
+
+            tokens = gensim.utils.simple_preprocess(text)
+
+
             print(len(text))
+            print(tokens)
 
-            doc = nlp(text)
-            for sent in doc.sents:
-
-                print(sent)
-            # print(file.readlines())
         break
 
         model = gensim.models.Word2Vec()
@@ -45,6 +46,15 @@ class MySentences(object):
     # i.e. how to recursively get files from every directory
     # an iterative solution can also work here: go into every directory
     def __iter__(self):
+
+        for filename in glob.iglob('../pubmed_data/**/*.txt', recursive=True):
+            with open(os.path.join("../pubmed_data/unzipped/AAPS_J", filename)) as file:
+                for line in file:
+                    yield gensim.utils.simple_preprocess(line)
+
+            print(filename)
+
+
         for fname in os.listdir(self.dirname):
             for line in open(os.path.join(self.dirname, fname)):
                 yield line.split()
